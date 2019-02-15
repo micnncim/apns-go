6f2419deacd77336a9990738b37d80175efd04b0
micnncim <micnncim@gmail.com>
2019-02-15 13:38:20 +0900

    Refactor client

--- main.go
+++ main.go
@@ -18,6 +18,38 @@ type Config struct {
 	Mode        string `toml:"mode"`
 }
 
+type Client struct {
+	apnsClient *apns2.Client
+	topic      string
+}
+
+func NewClient(certPath, password, topic, mode string) (*Client, error) {
+	cert, err := certificate.FromP12File(certPath, password)
+	if err != nil {
+		return nil, err
+	}
+	var client *apns2.Client
+	switch mode {
+	case "production":
+		client = apns2.NewClient(cert).Production()
+	default:
+		client = apns2.NewClient(cert).Development()
+	}
+	return &Client{
+		apnsClient: client,
+		topic:      topic,
+	}, err
+}
+
+func (c *Client) Push(message, deviceToken string) (*apns2.Response, error) {
+	payload := fmt.Sprintf(`{"aps":{"alert":"%s"}}`, message)
+	return c.apnsClient.Push(&apns2.Notification{
+		DeviceToken: deviceToken,
+		Topic:       c.topic,
+		Payload:     payload,
+	})
+}
+
 func main() {
 	var (
 		message = flag.String("message", "Hello!", "")
@@ -29,28 +61,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	cert, err := certificate.FromP12File(c.Filename, c.Password)
+	client, err := NewClient(c.Filename, c.Password, c.Topic, c.Mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := client.Push(*message, c.DeviceToken)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var client *apns2.Client
-	switch c.Mode {
-	case "production":
-		client = apns2.NewClient(cert).Production()
-	default:
-		client = apns2.NewClient(cert).Development()
-	}
-
-	res, err := client.Push(&apns2.Notification{
-		CollapseID:  "",
-		DeviceToken: c.DeviceToken,
-		Topic:       c.Topic,
-		Payload:     `{"aps":{"alert":"` + *message + `"}}`,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%v %v %v\n", res.StatusCode, res.ApnsID, res.Reason)
+	fmt.Printf("%v %v %v\n", resp.StatusCode, resp.ApnsID, resp.Reason)
 }
